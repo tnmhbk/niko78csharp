4d9c1d979c251f5b14f293c02bbbf70bf4a5b8b7
niko78 <niko78@ab31eaa2-35d0-11de-8e0c-53a27eea117e>
2009-06-04 04:20:21 +0000

    NLog Simple Demo

---
+++ trunk/VS2008/NLogSamples/Assemblies/README_NLog.txt
@@ -0,0 +1,6 @@
+----------------------------
+Readme NLog
+----------------------------
+
+Copy NLog.xsd to "c:\Program Files\Microsoft Visual Studio 8\Xml\Schemas\"
+to have Nlog.config VS intelisense.
---
+++ trunk/VS2008/NLogSamples/SimpleFileLogger/Program.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Say No More">
+//   2009
+// </copyright>
+// <summary>
+//   Simple logger demo program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SimpleFileLogger
+{
+    using NLog;
+
+    /// <summary>
+    /// Simple logger demo program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Nlog Logger.
+        /// </summary>
+        private static readonly Logger MyLogger = LogManager.GetLogger("MyLogger");
+
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
+        /// <param name="args">Comnad line Arguments.</param>
+        public static void Main(string[] args)
+        {
+            MyLogger.Debug("This is a simple logger !");
+            MyLogger.Info("This is a simple logger again!");
+        }
+    }
+}
