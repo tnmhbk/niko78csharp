815b035dc819d29752bcdbae1136d1beacb0e070
niko78 <niko78@ab31eaa2-35d0-11de-8e0c-53a27eea117e>
2009-11-06 06:24:56 +0000

    TraceSamples

---
+++ VS2008/TraceSamples/SimpleTrace/SimpleTraceSample.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SimpleTraceSample.cs" company="Say No More">
+//   2009
+// </copyright>
+// <summary>
+//   Simple Trace Sample.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SimpleTrace
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// Simple Trace Sample.
+    /// </summary>
+    public class SimpleTraceSample
+    {
+        /// <summary>
+        /// Entry point of Application.
+        /// </summary>
+        /// <param name="args">Start arguments.</param>
+        public static void Main(string[] args)
+        {
+            var thread = new Thread(TraceSamples) { IsBackground = true };
+            thread.Start();
+
+            Console.Write("Press enter to stop...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Prints periodically traces.
+        /// </summary>
+        public static void TraceSamples()
+        {
+            while (true)
+            {
+                Trace.WriteLine("Test Trace");
+                Trace.TraceError("Test Error");
+                Trace.TraceInformation("Test Information");
+                Debug.WriteLine("Test Debug");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
