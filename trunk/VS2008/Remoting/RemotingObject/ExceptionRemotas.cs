6fdd39f04daddfeb8016700195a28c7aa9b0b650
niko78 <niko78@ab31eaa2-35d0-11de-8e0c-53a27eea117e>
2009-12-02 05:19:02 +0000

    Remoting

---
+++ trunk/VS2008/Remoting/RemotingObject/ExceptionRemotas.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExceptionRemotas.cs" company="Say No More">
+//   2009
+// </copyright>
+// <summary>
+//   Excepcion remota
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RemotingObject
+{
+    using System.Runtime.Remoting;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Excepcion remota
+    /// </summary>
+    public class CustomRemotableException : RemotingException, ISerializable
+    {
+    }
+}
---
+++ trunk/VS2008/Remoting/RemotingObject/MyObjetoRemoto.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MyObjetoRemoto.cs" company="Say No More">
+//   2009
+// </copyright>
+// <summary>
+//   Defines the MyObjetoRemoto type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RemotingObject
+{
+    using System;
+
+    /// <summary>
+    /// Objeto remoto.
+    /// </summary>
+    public class MyObjetoRemoto : MarshalByRefObject
+    {
+        /// <summary>
+        /// Nombre del objeto.
+        /// </summary>
+        private string _nombre = "Sin Nombre";
+
+        /// <summary>
+        /// Devuelve el nombre del objeto.
+        /// </summary>
+        /// <returns>
+        /// El nombre del objeto.
+        /// </returns>
+        public string GetNombre()
+        {
+            return _nombre;
+        }
+
+        /// <summary>
+        /// Set el nombre del objeto.
+        /// </summary>
+        /// <param name="nombre">El nombre del objeto.
+        /// </param>
+        public void SetNombre(string nombre)
+        {
+            _nombre = nombre;
+        }
+    }
+}
