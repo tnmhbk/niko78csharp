e8f63ae77128e41df65433daf77a4e0037b4d474
niko78@gmail.com <niko78@gmail.com@ab31eaa2-35d0-11de-8e0c-53a27eea117e>
2011-09-10 05:50:30 +0000

    Primeros Views para modificar los Boxes.

--- trunk/Android/IFS Generator/src/com/niko78/IFSGenerator/MainActivity.java
+++ trunk/Android/IFS Generator/src/com/niko78/IFSGenerator/MainActivity.java
@@ -1,9 +1,16 @@
 package com.niko78.IFSGenerator;
 
+import com.niko78.IFSGenerator.Views.IFSView;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
+implements OnClickListener
 {
 
 	/** Called when the activity is first created. */
@@ -12,5 +19,24 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button button = (Button)findViewById(R.id.btnApply);
+        button.setOnClickListener(this);
     }
+   
+    // Implement the OnClickListener callback
+    public void onClick(View v) 
+    {
+    	EditText edtPosX = (EditText)findViewById(R.id.edPosX);
+    	EditText edtPosY = (EditText)findViewById(R.id.edPosY);
+    	IFSView ifsView = (IFSView)findViewById(R.id.ifsView);
+
+    	float posX = Float.parseFloat(edtPosX.getText().toString());
+    	float posY = Float.parseFloat(edtPosY.getText().toString());
+    	
+    	ifsView.SetSelectedBoxValues(posX, posY);
+    	
+    	ifsView.invalidate();
+    	
+    }    
 }
