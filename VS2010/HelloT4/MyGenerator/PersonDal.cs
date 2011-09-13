651f903393fe492896e3c8d3ef5fb87b2265b382
niko78 <niko78@gmail.com>
2011-09-13 04:56:24 +0000

    Hello T4.

---
+++ VS2010/HelloT4/MyGenerator/PersonDal.cs
@@ -0,0 +1,15 @@
+using MyClasses;
+
+namespace MyGenerator
+{
+    /// <summary>
+    /// Person Data Acess Layer
+    /// </summary>
+    public class PersonDal
+    {
+        public Person GetById(int id)
+        {
+            return null;
+        }
+    }
+}
