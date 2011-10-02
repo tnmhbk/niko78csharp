2eefa42d321b7675d700fd1a8efb90cc4f3572bc
niko78 <niko78@gmail.com>
2011-10-02 05:21:24 +0000

    - Implementacion de save to file.

--- Android/HelloAudioRecoder/src/com/Niko78/HelloAudio/Recoders/FileRecoder.java
+++ Android/HelloAudioRecoder/src/com/Niko78/HelloAudio/Recoders/FileRecoder.java
@@ -1,29 +1,39 @@
-package com.Niko78.HelloAudio;
+package com.Niko78.HelloAudio.Recoders;
 
-import com.Niko78.HelloAudio.Recoders.BaseRecoder;
-import com.Niko78.HelloAudio.Runners.WaveUpdater;
-import com.Niko78.HelloAudio.Views.WaveView;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Environment;
 
-public class Recorder extends BaseRecoder  
+public class FileRecoder extends BaseRecoder 
 {
-	private WaveView _associatedView;	
-	float average = 0.0F;	
 
-
-	public Recorder(WaveView associatedView) 
+	private File fileName;
+	
+	public FileRecoder()
 	{
 		super();
 		this.setFrequency(8000);
 		this.setChannelConfiguration(AudioFormat.CHANNEL_CONFIGURATION_MONO);
 		this.setPaused(false);
-		_associatedView = associatedView;		
+		
+		fileName = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/test.raw");		
 	}
-
-	public void CaptureSound() throws InterruptedException 
+	
+	public void run()
+	{
+		super.run();
+	}
+	
+	@Override
+	protected void CaptureSound() throws InterruptedException 
 	{
 		// Wait until we’re recording…
 		synchronized (mutex) 	
@@ -34,9 +44,37 @@ public class Recorder extends BaseRecoder
 			}
 		}
 
+		BufferedOutputStream bufferedStreamInstance = null;
+		
+		if (fileName.exists()) 
+		{
+			fileName.delete();
+		}
+		
+		try 
+		{
+			fileName.createNewFile();
+		} 
+		catch (IOException e) 
+		{
+			throw new IllegalStateException("Cannot create file: " + fileName.toString());
+		}
+		
+		try 
+		{
+			bufferedStreamInstance = new BufferedOutputStream(new FileOutputStream(this.fileName));
+		} 
+		catch (FileNotFoundException e) 
+		{
+			throw new IllegalStateException("Cannot Open File", e);
+		}
+		
+		DataOutputStream dataOutputStreamInstance = new DataOutputStream(bufferedStreamInstance);		
+		
+		
 		// We’re important…
 		android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-
+		
 		// 	Allocate Recorder and Start Recording…
 		int bufferRead = 0;
 		int bufferSize = AudioRecord.getMinBufferSize(this.getFrequency(), this.getChannelConfiguration(), this.getAudioEncoding());
@@ -44,8 +82,6 @@ public class Recorder extends BaseRecoder
 		bufferSize = 16000;
 		
 		AudioRecord recordInstance = new AudioRecord(MediaRecorder.AudioSource.MIC, this.getFrequency(), this.getChannelConfiguration(), this.getAudioEncoding(), bufferSize);
-
-		//int state = recordInstance.getState();
 
 		short[] tempBuffer = new short[bufferSize];
 		
@@ -80,23 +116,31 @@ public class Recorder extends BaseRecoder
 				throw new IllegalStateException("“read() returned AudioRecord.ERROR_INVALID_OPERATION");
 			}
 
-			average = 0.0F;
-			
-			for (int idxBuffer = 0; idxBuffer < bufferRead; ++idxBuffer) 
+			try 
 			{
-				average = average + tempBuffer[idxBuffer];
-			}
-			
-			average = average / bufferRead;
-			
-			WaveUpdater waveUpdater = new WaveUpdater(_associatedView, tempBuffer);
-
-			_associatedView.post(waveUpdater);
+				for (int idxBuffer = 0; idxBuffer < bufferRead; ++idxBuffer) 
+				{
+					dataOutputStreamInstance.writeShort(tempBuffer[idxBuffer]);
+				}
+			} 
+			catch (IOException e) 
+			{
+				throw new IllegalStateException("dataOutputStreamInstance.writeShort(curVal)");
+			}			
 		}
-
+		
 		// Close resources…
 		recordInstance.stop();
-	}
-
-
+		
+		try 
+		{
+			bufferedStreamInstance.close();
+		} 
+		catch (IOException e) 
+		{
+			throw new IllegalStateException("“Cannot close buffered writer.");
+		}
+		
+		
+	}		
 }
