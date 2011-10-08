5f955ab7f171d344504d629475630df177484043
niko78 <niko78@gmail.com>
2011-10-08 05:28:10 +0000

    Hello WorkFlow

---
+++ VS2010/HelloWorkflow/HelloWorkflow/XmlWorkflowInstanceStore .cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Activities.DurableInstancing;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.DurableInstancing;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace HelloWorkflow
+{
+    public class XmlWorkflowInstanceStore : InstanceStore
+    {
+        Guid ownerInstanceID;
+
+        public XmlWorkflowInstanceStore()
+            : this(Guid.NewGuid())
+        {
+
+        }
+
+        public XmlWorkflowInstanceStore(Guid id)
+        {
+            ownerInstanceID = id;
+        }
+
+        //Synchronous version of the Begin/EndTryCommand functions
+        protected override bool TryCommand(InstancePersistenceContext context, InstancePersistenceCommand command, TimeSpan timeout)
+        {
+            return EndTryCommand(BeginTryCommand(context, command, timeout, null, null));
+        }
+
+        //The persistence engine will send a variety of commands to the configured InstanceStore,
+        //such as CreateWorkflowOwnerCommand, SaveWorkflowCommand, and LoadWorkflowCommand.
+        //This method is where we will handle those commands
+        protected override IAsyncResult BeginTryCommand(InstancePersistenceContext context, InstancePersistenceCommand command, TimeSpan timeout, AsyncCallback callback, object state)
+        {
+            IDictionary<XName, InstanceValue> data = null;
+
+            //The CreateWorkflowOwner command instructs the instance store to create a new instance owner bound to the instanace handle
+            if (command is CreateWorkflowOwnerCommand)
+            {
+                context.BindInstanceOwner(ownerInstanceID, Guid.NewGuid());
+            }
+            //The SaveWorkflow command instructs the instance store to modify the instance bound to the instance handle or an instance key
+            else if (command is SaveWorkflowCommand)
+            {
+                SaveWorkflowCommand saveCommand = (SaveWorkflowCommand)command;
+                data = saveCommand.InstanceData;
+
+                Save(data);
+            }
+            //The LoadWorkflow command instructs the instance store to lock and load the instance bound to the identifier in the instance handle
+            else if (command is LoadWorkflowCommand)
+            {
+                string fileName = this.ownerInstanceID.ToString();
+
+                try
+                {
+                    using (FileStream inputStream = new FileStream(fileName, FileMode.Open))
+                    {
+                        data = LoadInstanceDataFromFile(inputStream);
+                        //load the data into the persistence Context
+                        context.LoadedInstance(InstanceState.Initialized, data, null, null, null);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception(exception.Message);
+                }
+            }
+
+            return new CompletedAsyncResult<bool>(true, callback, state);
+        }
+
+        protected override bool EndTryCommand(IAsyncResult result)
+        {
+            return CompletedAsyncResult<bool>.End(result);
+        }
+
+        //Reads data from xml file and creates a dictionary based off of that.
+        IDictionary<XName, InstanceValue> LoadInstanceDataFromFile(Stream inputStream)
+        {
+            IDictionary<XName, InstanceValue> data = new Dictionary<XName, InstanceValue>();
+
+            NetDataContractSerializer s = new NetDataContractSerializer();
+
+            XmlReader rdr = XmlReader.Create(inputStream);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(rdr);
+
+            XmlNodeList instances = doc.GetElementsByTagName("InstanceValue");
+            foreach (XmlElement instanceElement in instances)
+            {
+                XmlElement keyElement = (XmlElement)instanceElement.SelectSingleNode("descendant::key");
+                XName key = (XName)DeserializeObject(s, keyElement);
+
+                XmlElement valueElement = (XmlElement)instanceElement.SelectSingleNode("descendant::value");
+                object value = DeserializeObject(s, valueElement);
+                InstanceValue instVal = new InstanceValue(value);
+
+                data.Add(key, instVal);
+            }
+
+            return data;
+        }
+
+        object DeserializeObject(NetDataContractSerializer serializer, XmlElement element)
+        {
+            object deserializedObject = null;
+
+            MemoryStream stm = new MemoryStream();
+            XmlDictionaryWriter wtr = XmlDictionaryWriter.CreateTextWriter(stm);
+            element.WriteContentTo(wtr);
+            wtr.Flush();
+            stm.Position = 0;
+
+            deserializedObject = serializer.Deserialize(stm);
+
+            return deserializedObject;
+        }
+
+        //Saves the persistence data to an xml file.
+        void Save(IDictionary<XName, InstanceValue> instanceData)
+        {
+            string fileName = this.ownerInstanceID.ToString();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<InstanceValues/>");
+
+            foreach (KeyValuePair<XName, InstanceValue> valPair in instanceData)
+            {
+                XmlElement newInstance = doc.CreateElement("InstanceValue");
+
+                XmlElement newKey = SerializeObject("key", valPair.Key, doc);
+                newInstance.AppendChild(newKey);
+
+                XmlElement newValue = SerializeObject("value", valPair.Value.Value, doc);
+                newInstance.AppendChild(newValue);
+
+                doc.DocumentElement.AppendChild(newInstance);
+            }
+            doc.Save(fileName);
+        }
+
+        XmlElement SerializeObject(string elementName, object o, XmlDocument doc)
+        {
+            NetDataContractSerializer s = new NetDataContractSerializer();
+            XmlElement newElement = doc.CreateElement(elementName);
+            MemoryStream stm = new MemoryStream();
+
+            s.Serialize(stm, o);
+            stm.Position = 0;
+            StreamReader rdr = new StreamReader(stm);
+            newElement.InnerXml = rdr.ReadToEnd();
+
+            return newElement;
+        }
+    }
+
+    /// <summary>
+    /// A generic base class for IAsyncResult implementations
+    /// that wraps a ManualResetEvent.
+    /// </summary>
+    abstract class AsyncResult : IAsyncResult
+    {
+        AsyncCallback callback;
+        object state;
+        bool completedSynchronously;
+        bool endCalled;
+        Exception exception;
+        bool isCompleted;
+        ManualResetEvent manualResetEvent;
+        object thisLock;
+
+        protected AsyncResult(AsyncCallback callback, object state)
+        {
+            this.callback = callback;
+            this.state = state;
+            this.thisLock = new object();
+        }
+
+        public object AsyncState
+        {
+            get
+            {
+                return state;
+            }
+        }
+
+        public WaitHandle AsyncWaitHandle
+        {
+            get
+            {
+                if (manualResetEvent != null)
+                {
+                    return manualResetEvent;
+                }
+
+                lock (ThisLock)
+                {
+                    if (manualResetEvent == null)
+                    {
+                        manualResetEvent = new ManualResetEvent(isCompleted);
+                    }
+                }
+
+                return manualResetEvent;
+            }
+        }
+
+        public bool CompletedSynchronously
+        {
+            get
+            {
+                return completedSynchronously;
+            }
+        }
+
+        public bool IsCompleted
+        {
+            get
+            {
+                return isCompleted;
+            }
+        }
+
+        object ThisLock
+        {
+            get
+            {
+                return this.thisLock;
+            }
+        }
+
+        // Call this version of complete when your asynchronous operation is complete.  This will update the state
+        // of the operation and notify the callback.
+        protected void Complete(bool completedSynchronously)
+        {
+            if (isCompleted)
+            {
+                // It's a bug to call Complete twice.
+                throw new InvalidOperationException("Cannot call Complete twice");
+            }
+
+            this.completedSynchronously = completedSynchronously;
+
+            if (completedSynchronously)
+            {
+                // If we completedSynchronously, then there's no chance that the manualResetEvent was created so
+                // we don't need to worry about a race
+                this.isCompleted = true;
+            }
+            else
+            {
+                lock (ThisLock)
+                {
+                    this.isCompleted = true;
+                    if (this.manualResetEvent != null)
+                    {
+                        this.manualResetEvent.Set();
+                    }
+                }
+            }
+
+            // If the callback throws, there is a bug in the callback implementation
+            if (callback != null)
+            {
+                callback(this);
+            }
+        }
+
+        // Call this version of complete if you raise an exception during processing.  In addition to notifying
+        // the callback, it will capture the exception and store it to be thrown during AsyncResult.End.
+        protected void Complete(bool completedSynchronously, Exception exception)
+        {
+            this.exception = exception;
+            Complete(completedSynchronously);
+        }
+
+        // End should be called when the End function for the asynchronous operation is complete.  It
+        // ensures the asynchronous operation is complete, and does some common validation.
+        protected static TAsyncResult End<TAsyncResult>(IAsyncResult result)
+            where TAsyncResult : AsyncResult
+        {
+            if (result == null)
+            {
+                throw new ArgumentNullException("result");
+            }
+
+            TAsyncResult asyncResult = result as TAsyncResult;
+
+            if (asyncResult == null)
+            {
+                throw new ArgumentException("Invalid async result.", "result");
+            }
+
+            if (asyncResult.endCalled)
+            {
+                throw new InvalidOperationException("Async object already ended.");
+            }
+
+            asyncResult.endCalled = true;
+
+            if (!asyncResult.isCompleted)
+            {
+                asyncResult.AsyncWaitHandle.WaitOne();
+            }
+
+            if (asyncResult.manualResetEvent != null)
+            {
+                asyncResult.manualResetEvent.Close();
+            }
+
+            if (asyncResult.exception != null)
+            {
+                throw asyncResult.exception;
+            }
+
+            return asyncResult;
+        }
+    }
+
+    //An AsyncResult that completes as soon as it is instantiated.
+    class CompletedAsyncResult : AsyncResult
+    {
+        public CompletedAsyncResult(AsyncCallback callback, object state)
+            : base(callback, state)
+        {
+            Complete(true);
+        }
+
+        public static void End(IAsyncResult result)
+        {
+            AsyncResult.End<CompletedAsyncResult>(result);
+        }
+    }
+
+    //A strongly typed AsyncResult
+    abstract class TypedAsyncResult<T> : AsyncResult
+    {
+        T data;
+
+        protected TypedAsyncResult(AsyncCallback callback, object state)
+            : base(callback, state)
+        {
+        }
+
+        public T Data
+        {
+            get { return data; }
+        }
+
+        protected void Complete(T data, bool completedSynchronously)
+        {
+            this.data = data;
+            Complete(completedSynchronously);
+        }
+
+        public static T End(IAsyncResult result)
+        {
+            TypedAsyncResult<T> typedResult = AsyncResult.End<TypedAsyncResult<T>>(result);
+            return typedResult.Data;
+        }
+    }
+
+    //A strongly typed AsyncResult that completes as soon as it is instantiated.
+    class TypedCompletedAsyncResult<T> : TypedAsyncResult<T>
+    {
+        public TypedCompletedAsyncResult(T data, AsyncCallback callback, object state)
+            : base(callback, state)
+        {
+            Complete(data, true);
+        }
+
+        public new static T End(IAsyncResult result)
+        {
+            TypedCompletedAsyncResult<T> completedResult = result as TypedCompletedAsyncResult<T>;
+            if (completedResult == null)
+            {
+                throw new ArgumentException("Invalid async result.", "result");
+            }
+
+            return TypedAsyncResult<T>.End(completedResult);
+        }
+    }
+
+    class CompletedAsyncResult<T> : AsyncResult
+    {
+        T data;
+
+        public CompletedAsyncResult(T data, AsyncCallback callback, object state)
+            : base(callback, state)
+        {
+            this.data = data;
+            Complete(true);
+        }
+
+        public static T End(IAsyncResult result)
+        {
+            CompletedAsyncResult<T> completedResult = AsyncResult.End<CompletedAsyncResult<T>>(result);
+            return completedResult.data;
+        }
+    }
+
+    
+}
