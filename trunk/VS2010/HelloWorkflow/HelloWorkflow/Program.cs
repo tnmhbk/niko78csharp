4e37d691cc3aae182810debb41b65908802d5d76
niko78@gmail.com <niko78@gmail.com@ab31eaa2-35d0-11de-8e0c-53a27eea117e>
2011-10-08 05:28:10 +0000

    Hello WorkFlow

---
+++ trunk/VS2010/HelloWorkflow/HelloWorkflow/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace HelloWorkflow
+{
+    class Program
+    {
+        private static WorkflowApplication _application;
+
+        private static AutoResetEvent idleEvent = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" --- Inicio de programa ---");
+
+            _application = CreateNewWorkFlow();
+            _application.Completed += OnCompleted;
+            _application.InstanceStore = new XmlWorkflowInstanceStore();
+            _application.Run();
+
+            idleEvent.WaitOne();
+
+            Console.WriteLine(" --- Fin de programa ---");
+            Console.ReadLine();
+
+        }
+
+        private static WorkflowApplication CreateNewWorkFlow()
+        {
+            WorkflowApplication wfApp = new WorkflowApplication(new MainActivity());
+            return wfApp;
+        }
+
+        public static void OnCompleted(WorkflowApplicationCompletedEventArgs e)
+        {
+            idleEvent.Set();
+        }
+    }
+}
