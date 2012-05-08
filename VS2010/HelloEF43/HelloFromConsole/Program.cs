5729dac49445da9f69afcb43eafd0dbfc0cc0998
niko78 <niko78@gmail.com>
2012-05-08 02:07:36 +0000

    - HelloEF43

---
+++ VS2010/HelloEF43/HelloFromConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace HelloFromConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ProductContext())
+            {
+                // Add a food category 
+                var food = new Category { CategoryId = "FOOD", Name = "Foods" };
+                db.Categories.Add(food);
+                int recordsAffected = db.SaveChanges();
+                Console.WriteLine("Saved {0} entities to the database, press any key to exit.", recordsAffected);
+                Console.ReadKey();
+            } 
+        }
+    }
+
+    public class Category
+    {
+        public string CategoryId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+
+    public class ProductContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
