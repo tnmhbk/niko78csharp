6e678ca124a793083f968001746d5bc2c1f20c7a
niko78@gmail.com <niko78@gmail.com@ab31eaa2-35d0-11de-8e0c-53a27eea117e>
2012-05-08 02:10:52 +0000

    HelloEF41

---
+++ trunk/VS2010/HelloEF41/Dal/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Dal
+{
+    public class User
+    {
+        public User()
+        {
+            Movements = new List<Movement>();
+        }
+
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public ICollection<Movement> Movements { get; set; }
+    }
+}
---
+++ trunk/VS2010/HelloEF41/DalTest/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Dal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DalTest
+{
+    [TestClass]
+    public class TestUser
+    {
+        [TestMethod]
+        public void InitializeDatabse()
+        {
+            HelloContext helloContext = new HelloContext();
+            HelloContextInitializaer helloContextInitializaer = new HelloContextInitializaer();
+            helloContextInitializaer.InitializeDatabase(helloContext);
+        }
+
+        [TestMethod]
+        public void TestCreate()
+        {
+
+            HelloContext helloContext = new HelloContext();
+
+            User user = new User();
+            user.Nombre = "Nico";
+
+            Movement movement = new Movement();
+            movement.CreationTime = DateTime.Now;
+
+            user.Movements.Add(movement);
+            
+
+            helloContext.Users.Add(user);
+
+            helloContext.SaveChanges();
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            HelloContext helloContext = new HelloContext();
+            User user = helloContext.Users.Include("Movements").First();
+
+            Assert.AreEqual(typeof(User), user.GetType());
+        }
+    }
+}
